a633f4e61c79fbf29db1865ccef1deff31d355a5
Colin Sidberry <colinsidberry@Colins-MBP-2.attlocal.net>
2024-10-29 22:24:30 -0700

    removing unused import.

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React, {useState, useEffect} from 'react';
-import Image from "next/image";
 import {HiSearch, HiMenu} from "react-icons/hi";
 import {useSession, signIn, signOut} from "next-auth/react"
 import {doc, getFirestore, setDoc} from "@firebase/firestore";
